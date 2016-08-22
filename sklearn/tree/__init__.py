90ab7c63d1486b7ba9b2df4dc28d9c5076ddeffa
alexsyrom <alexsyrom@yandex.ru>
2016-08-22 16:57:12 +0300

    __init__

--- sklearn/tree/__init__.py
+++ sklearn/tree/__init__.py
@@ -7,7 +7,8 @@ from .tree import DecisionTreeClassifier
 from .tree import DecisionTreeRegressor
 from .tree import ExtraTreeClassifier
 from .tree import ExtraTreeRegressor
+from .lin_reg_tree import LinearDecisionTreeRegressor
 from .export import export_graphviz
 
 __all__ = ["DecisionTreeClassifier", "DecisionTreeRegressor",
-           "ExtraTreeClassifier", "ExtraTreeRegressor", "export_graphviz"]
+           "ExtraTreeClassifier", "ExtraTreeRegressor", "LinearDecisionTreeRegressor", "export_graphviz"]
